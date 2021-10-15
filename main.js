8a7b3675834249dc72ec8c2aa34a6b0361f52e68
sirsavant <stanleyho619@gmail.com>
2021-10-15 15:12:46 -0400

    update files

---
+++ main.js
@@ -0,0 +1,24 @@
+// setup canvas
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate a random number
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// constructor function
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype
