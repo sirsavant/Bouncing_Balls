cdfd2545b2af74e878b70c8bd741f303799ea287
sirsavant <stanleyho619@gmail.com>
2021-10-18 13:35:23 -0400

    finished demo

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// source: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_building_practice
+
 // setup canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -21,4 +23,82 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-Ball.prototype
+// Ball.draw method to create the object
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+// Ball.update method to move the ball
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+// collision detection
+Ball.prototype.collisionDetect = function() {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")";
+      }
+    }
+  }
+}
+
+
+// creating new balls 
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10, 30);
+  let ball = new Ball(
+    // The ball position is always drawn at least one ball width away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    "rgb(" + random(0, 255) + "," + random(0, 255) + ',' + random(0, 255) + ")",
+    size
+  );
+
+  balls.push(ball);
+}
+
+// loop function to add the black background and call the draw and update function for each ball. Then the requestAnimationFrame method tells the browser to call a specified function to update an animation before the next repaint.
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
